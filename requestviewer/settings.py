1b14e6cc74e100ac16047325fc530bbfdd8a0290
Kanae Usui <usuikanae@gmail.com>
2020-07-24 06:29:10 +0900

    production ready

--- requestviewer/settings.py
+++ requestviewer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0$j$*c(p!jf1_wg+gvovbi2cxzx#st7=m3ky&oxzw*pszrcvo5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
