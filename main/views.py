4f12d12c2b4814d95addac78911dacbc1dbc3e3b
Kanae Usui <d2vmIvjZU0vN58l@live.jp>
2022-05-17 22:54:46 +0900

    Added sleep option

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import Http404
-
+import time
 # Create your views here.
 class IndexView(generic.CreateView):
 
@@ -21,6 +21,20 @@ class IndexView(generic.CreateView):
         context['headers'] = dict(request.headers)
         print(context['headers'])
 
+        # basic authentication
+        # if "auth" in request.GET:
+
+        # Sleep
+        if "sleep" in request.GET:
+            print("Sleep option")
+            sleep_val_str = request.GET.get("sleep")
+            if sleep_val_str.isdigit():
+                sleep_value = int(sleep_val_str)
+                if sleep_value > 0:
+                    print("Sleep ", sleep_value, "seconds")
+                    time.sleep(sleep_value)
+
+        # websocket(todo)
         return render(request, 'index.html', context)
 
     def post(self, request, *args, **kwargs):
