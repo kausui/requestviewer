fa33d1152975843b49d295e00c52c7795d332e30
Kanae Usui <d2vmIvjZU0vN58l@live.jp>
2022-05-20 00:14:44 +0900

    context method sleep method add post data display

--- main/views.py
+++ main/views.py
@@ -6,12 +6,24 @@ import time
 # Create your views here.
 class IndexView(generic.CreateView):
     def get(self, request, *args, **kwargs):
-        print(request.META['REMOTE_ADDR'])
-        print(request.META['SERVER_PORT'])
-        print(request.method)
-        print(request.path)
-        print(request.headers)
+        context = self.prepare_context(request)
 
+        # basic authentication(todo)
+        # if "auth" in request.GET:
+
+        self.sleep(request)
+
+        # websocket(todo)
+        return render(request, 'index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.prepare_context(request)
+        self.sleep(request)
+        context['post'] = dict(request.POST)
+
+        return render(request, 'index.html', context)
+
+    def prepare_context(self, request):
         context = dict()
         context['clientip'] = request.META['REMOTE_ADDR']
         context['serverport'] = request.META['SERVER_PORT']
@@ -20,10 +32,9 @@ class IndexView(generic.CreateView):
         context['headers'] = dict(request.headers)
         print(context['headers'])
 
-        # basic authentication
-        # if "auth" in request.GET:
+        return context
 
-        # Sleep
+    def sleep(self, request):
         if "sleep" in request.GET:
             print("Sleep option")
             sleep_val_str = request.GET.get("sleep")
@@ -32,36 +43,6 @@ class IndexView(generic.CreateView):
                 if sleep_value > 0:
                     print("Sleep ", sleep_value, "seconds")
                     time.sleep(sleep_value)
-
-        # websocket(todo)
-        return render(request, 'index.html', context)
-
-    def post(self, request, *args, **kwargs):
-        print(request.META['REMOTE_ADDR'])
-        print(request.META['SERVER_PORT'])
-        print(request.method)
-        print(request.path)
-        print(request.headers)
-
-        context = dict()
-        context['clientip'] = request.META['REMOTE_ADDR']
-        context['serverport'] = request.META['SERVER_PORT']
-        context['path'] = request.path
-        context['method'] = request.method
-        context['headers'] = dict(request.headers)
-        print(context['headers'])
-
-        # Sleep
-        if "sleep" in request.GET:
-            print("Sleep option")
-            sleep_val_str = request.GET.get('sleep')
-            if sleep_val_str.isdigit():
-                sleep_value = int(sleep_val_str)
-                if sleep_value > 0:
-                    print("Sleep ", sleep_value, "seconds")
-                    time.sleep(sleep_value)
-
-        return render(request, 'index.html', context)
 
 class PageNotFoundView(generic.CreateView):
     def get(self, request, *args, **kwargs):
