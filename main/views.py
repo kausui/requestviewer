0590af7c29368c6e7fefcee85a7dea10b68d266c
Kanae Usui <d2vmIvjZU0vN58l@live.jp>
2022-06-26 01:02:19 +0900

    implement status code

--- main/views.py
+++ main/views.py
@@ -7,22 +7,33 @@ import time
 class IndexView(generic.CreateView):
     def get(self, request, *args, **kwargs):
         context = self.prepare_context(request)
-
+        status_code = 200
         # basic authentication(todo)
         # if "auth" in request.GET:
 
         self.sleep(request)
 
+        if "status" in request.GET:
+            print("Status Code option")
+            status_val_str = request.GET.get("status")
+            if status_val_str.isdigit():
+                status_value = int(status_val_str)
+                if status_value > 0:
+                    print("Status code: ", status_value)
+                    status_code = status_value
+
         # websocket(todo)
-        return render(request, 'index.html', context)
+        return render(request, 'index.html', context, status=status_code)
 
     def post(self, request, *args, **kwargs):
         context = self.prepare_context(request)
         self.sleep(request)
         self.post_sleep(request)
+        status_code = 200
+        status_code = self.post_status(request)
         context['post'] = request.POST
 
-        return render(request, 'index.html', context)
+        return render(request, 'index.html', context, status=status_code)
 
     def prepare_context(self, request):
         context = dict()
@@ -54,6 +65,16 @@ class IndexView(generic.CreateView):
                 if sleep_value > 0:
                     print("Sleep ", sleep_value, "seconds")
                     time.sleep(sleep_value)
+
+    def post_status(self, request):
+        if "status" in request.POST:
+            print("POST Status option")
+            status_val_str = request.POST.get("status")
+            if status_val_str.isdigit():
+                status_value = int(status_val_str)
+                if status_value > 0:
+                    print("Status Code: ", status_value)
+                    return status_value
 
 class PageNotFoundView(generic.CreateView):
     def get(self, request, *args, **kwargs):
